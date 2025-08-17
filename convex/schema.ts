1c97c6725efd833ed65c730888239e0774698074
vandelooij.tom <tomjurgen@proton.me>
2025-08-17 13:35:19 +0200

    add search bar

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,5 +6,12 @@ export default defineSchema({
         nickname: v.string(),
         email: v.string(),
         updatedAt: v.number()
+    }),
+    friendships: defineTable({
+        userId1: v.id("users"),
+        userId2: v.id("users"),
+        status: v.string(), // "pending", "accepted", "blocked"
+        createdAt: v.number(),
+        updatedAt: v.number()
     })
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { Authenticated, useConvexAuth, useMutation } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useConvexAuth,
+  useMutation,
+} from "convex/react";
 import { api } from "../../convex/_generated/api";
 
-import { useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const auth = useConvexAuth();
@@ -18,6 +24,11 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Unauthenticated>
+          <SignInButton>
+            <Button variant="ghost">Inloggen</Button>
+          </SignInButton>
+        </Unauthenticated>
         <Authenticated>hello {user?.username}</Authenticated>
       </main>
     </div>
