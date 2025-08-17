63565f257815a4d63a4e63d4dacb0df3341198b0
vandelooij.tom <tomjurgen@proton.me>
2025-08-17 12:06:26 +0200

    integrate convex with clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useQuery, Authenticated } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Authenticated>
+          {tasks?.map(({ _id, text }) => (
+            <div key={_id}>{text}</div>
+          ))}
+        </Authenticated>
+      </main>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Authenticated, Unauthenticated } from "convex/react";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
@@ -6,19 +9,19 @@ export function Header() {
   return (
     <header className="flex h-16 items-center justify-between gap-4 border-b px-4">
       <Link href="/" className="flex items-center gap-x-4">
-        <span className="font-sans text-xl font-semibold text-yellow-300">
+        <span className="font-sans text-2xl font-bold text-yellow-300">
           foyyer
         </span>
       </Link>
       <div className="flex items-center gap-x-4">
-        <SignedOut>
+        <Unauthenticated>
           <SignInButton>
             <Button variant="ghost">Inloggen</Button>
           </SignInButton>
-        </SignedOut>
-        <SignedIn>
+        </Unauthenticated>
+        <Authenticated>
           <UserButton />
-        </SignedIn>
+        </Authenticated>
       </div>
     </header>
   );
